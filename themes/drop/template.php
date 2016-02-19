b418090c65349fe27cd939fed62216a1d608f698
Wes Ruvalcaba <wesruv@gmail.com>
2016-02-19 00:45:42 -0500

    Themed tabs a bit

---
+++ themes/drop/template.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Implements template_preprocess_page
+ */
+function drop_preprocess_layout(&$variables) {
+  $theme_path = backdrop_get_path('theme', 'drop');
+  $seven_theme_path = backdrop_get_path('theme', 'seven');
+
+  if (!empty($variables['tabs'])) {
+    backdrop_add_css($theme_path . '/css/admin-tabs.css');
+    backdrop_add_js($seven_theme_path . '/js/script.js');
+  }
+}
