78d8dd77798c51382bdcc8f9b36751dcec9c0452
Wes Ruvalcaba <wesruv@gmail.com>
2016-02-28 18:47:09 -0500

    Finished styling header for mobile and desktop, homepage layout looks done

--- themes/drop/template.php
+++ themes/drop/template.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Implements template_preprocess_page
+ * Implements template_preprocess_page().
  */
 function drop_preprocess_layout(&$variables) {
   $theme_path = backdrop_get_path('theme', 'drop');
@@ -15,4 +15,22 @@ function drop_preprocess_layout(&$variables) {
   if ($variables['is_front']) {
     $variables['classes'][] = 'layout-front';
   }
+}
+
+/**
+ * Overrides theme_breadcrumb().
+ * Removing raquo from markup
+ */
+function drop_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  $output = '';
+  if (!empty($breadcrumb)) {
+    $output .= '<nav role="navigation" class="breadcrumb">';
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output .= '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+    $output .= '<ol><li>' . implode('</li><li>', $breadcrumb) . '</li></ol>';
+    $output .= '</nav>';
+  }
+  return $output;
 }
